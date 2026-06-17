5b4de6db4f4024e6bb6aa2c9fb17e7028f190050
OULEDZIAN <(ouledahmedzian)>
2026-06-17 03:13:38 +0200

    chore: scaffold Xcode project (SwiftUI, Swift Testing)

---
+++ BankApp/BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by AOZ on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankApp/BankAppTests/BankAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  BankAppTests.swift
+//  BankAppTests
+//
+//  Created by AOZ on 17/06/2026.
+//
+
+import Testing
+
+struct BankAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
